db659f30163065c5ad87b84a627b473ae0de8e64
Alex <alex_mcgarry@hotmail.co.uk>
2023-06-02 18:57:52 +0100

    fix loading

--- src/components/AllReviews.jsx
+++ src/components/AllReviews.jsx
@@ -1,36 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import * as api from '../api';
 import '../styles/AllReviews.css';
 import ReviewCard from './ReviewCard';
 
-const AllReviews = () => {
-  const [reviews, setReviews] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    api
-      .getReviews()
-      .then((res) => {
-        setReviews(res.data.reviews);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setIsLoading(false);
-      });
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading reviews...</p>;
-  }
-
-  return (
-    <div className="reviews-container">
-      {reviews.map((review) => (
-        <ReviewCard key={review.review_id} review={review} />
-      ))}
-    </div>
-  );
-};
+const AllReviews = ({ reviews }) => (
+  <div className="reviews-container">
+    {reviews.map((review) => (
+      <ReviewCard key={review.review_id} review={review} />
+    ))}
+  </div>
+);
 
 export default AllReviews;
